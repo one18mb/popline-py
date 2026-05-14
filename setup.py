6b74a707d9c71a351f7ca84f71d718f6662857a0
fz <1915182174@qq.com>
2026-05-14 18:31:43 +0800

    v0.4.1: fix sdist — include popline_sax.c in MANIFEST.in

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ module = Extension(
 
 setup(
     name='popline-py',
-    version='0.4.0',
+    version='0.4.1',
     description='PopLine — Line-oriented serialization format (C extension)',
     long_description=long_desc,
     long_description_content_type='text/markdown',
